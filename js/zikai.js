cd120c8b4ff75483c07d8fd98e5c54594e83b945
liuzikai <zikai.liu@outlook.com>
2022-08-07 00:47:46 +0200

    change: replace JQuery with vanilla JS

--- js/zikai.js
+++ js/zikai.js
@@ -1,7 +1,7 @@
 $(function () {
 
-    // https://stackoverflow.com/a/12418814/10087792
-    function inViewport (element) {
+    // Reference: https://stackoverflow.com/a/12418814/10087792
+    function inViewport(element) {
         if (!element) return false;
         if (1 !== element.nodeType) return false;
 
@@ -15,15 +15,31 @@ $(function () {
             rect.top <= html.clientHeight;
     }
 
-    let allSlideInTexts = $(".slide-in-text");
-    $.each(allSlideInTexts, function () {
-        if ($(this).visible(true)) {
-            $(this).addClass("come-in");
+    // Get the array of all .slide-in-text
+    let allSlideInTexts = [];
+    for (const el of document.getElementsByClassName("slide-in-text")) {
+        allSlideInTexts.push(el);
+    }
+
+    // Slide in animation
+    function updateSlideInTexts() {
+        for (var i = 0; i < allSlideInTexts.length; i++) {
+            let e = allSlideInTexts[i];
+            if (inViewport(e)) {
+                e.classList.add("come-in");
+                // Remove it from the list
+                allSlideInTexts.splice(i, 1);
+                i--;
+            }
         }
-    });
+    }
+
+    updateSlideInTexts();  // run once when DOM is ready
+
+    // Functions for SVG animation
 
     function clamp(num, min, max) {
-        return num <= min ? min : (num >= max ? max : num)
+        return num <= min ? min : (num >= max ? max : num);
     }
 
     function percentageEase(percentage) {
@@ -33,62 +49,80 @@ $(function () {
     function calcProjectSVGPercentage(projectElem) {
         // When clientRect.top = window.innerHeight, 0%
         // When clientRect.top = navBarHeight, 100%
-        let alpha = -1 / (window.innerHeight - $("#mainNav").innerHeight())
-        let percentage = (projectElem.getBoundingClientRect().top - window.innerHeight) * alpha
+        let alpha = -1 / (window.innerHeight - document.getElementById("mainNav").offsetHeight);
+        let percentage = (projectElem.getBoundingClientRect().top - window.innerHeight) * alpha;
         if (percentage < 0) return 0;
-        return percentageEase(clamp(percentage, 0, 1))
+        return percentageEase(clamp(percentage, 0, 1));
     }
 
-    function drawProjectSVG(svgSel, percentage) {
-        let dashTotal = parseInt(svgSel.css("stroke-dasharray"), 10);
-        svgSel.css("stroke-dashoffset", dashTotal * (1 - percentage));
+    function drawProjectSVG(svgElem, dashTotal, percentage) {
+        svgElem.style.strokeDashoffset = dashTotal * (1 - percentage);
+    }
+
+    // SVG information: {svg element => [container element, stroke-dashoffset]}
+    let svgInfo = new Map();
+
+    // Update all SVGs
+    function updateSVGs() {
+        for (const [key, value] of svgInfo.entries()) {
+            drawProjectSVG(key, value[1], calcProjectSVGPercentage(value[0]));
+        }
     }
 
     function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
 
-    let allProjectContainers = $(".project-container");
-
-    async function driveProjectSVG(svgSel, current, target) {
+    // Async function to drive an SVG to a certain percentage with delay (animation)
+    // Used to drive a svg when it first shows up, when user refresh the page and it already shows up in the viewpoint
+    async function driveSVG(svgElem, dashTotal, current, target) {
         if (current > target) current = target;
-        drawProjectSVG(svgSel, current);
+        drawProjectSVG(svgElem, dashTotal, current);
         if (current < target) {
-            // Forward 1% per 20ms (take 2s from 0% to 100%)
-            await sleep(20);
-            await driveProjectSVG(svgSel, current + 0.01, target);
+            // Forward 2% per 50ms (take 2.5s from 0% to 100%)
+            await sleep(50);
+            await driveSVG(svgElem, dashTotal, current + 0.02, target);
         }
     }
 
-    // Dynamically load files as inline source (necessary for svg animation)
-    $.each($('[data-include]'), function () {
-        $(this).load($(this).data('include'), function() {
-            // If the svg is already visible, drive the animation using timer
-            let projectSel = $(this).parent().closest(".project-container")
-            if (projectSel.length > 0) {
-                let percentage = calcProjectSVGPercentage(projectSel.get(0));
-                if (percentage > 0) {
-                    let svgSel = $(this).find("[id$=animated-svg]");
-                    driveProjectSVG(svgSel, 0, percentage).then(_ => {});
-                }
-            }
-        })
-    })
+    for (const e of document.getElementsByClassName("icon-placeholder")) {
+        // Fetch the content
+        let request = new XMLHttpRequest();
+        request.open('GET', e.dataset.include, true);
+        request.onload = function () {
+            if (request.status >= 200 && request.status < 400) {
+                e.innerHTML = request.responseText;
 
+                // Find the container
+                let container = e.closest(".project-container-down-lg");
+                if (!container) {
+                    container = e.closest(".project-container");
+                }
+
+                // Iterate the path inside
+                Array.from(e.querySelectorAll("path")).forEach(svg => {
+                    if (svg.id.endsWith("-animated-svg")) {
+                        // Get the stroke-dashoffset
+                        let style = window.getComputedStyle(svg);
+                        let len = parseInt(style.getPropertyValue("stroke-dashoffset"), 10);
+
+                        // Store the information
+                        svgInfo.set(svg, [container, len]);
+
+                        // First-time show up, animate it to the current point if already in the view point
+                        driveSVG(svg, len, 0, calcProjectSVGPercentage(container)).then();
+                    }
+                });
+            }
+        };
+        request.send();
+    }
+    // Note: svgInfo is updated async
 
     // On a scroll event
-    $(window).scroll(function () {
-        $.each(allProjectContainers, function () {
-            let svgSel = $(this).find("[id$=animated-svg]");
-            drawProjectSVG(svgSel, calcProjectSVGPercentage(this))
-        })
-
-        $.each(allSlideInTexts, function () {
-            if ($(this).visible(true)) {
-                $(this).addClass("come-in");
-            }
-        });
-
+    document.addEventListener('scroll', function (e) {
+        updateSVGs();
+        updateSlideInTexts();
     });
 })
 
