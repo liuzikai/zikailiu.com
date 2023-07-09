cd3407df1a2a2396bb62981365e3f9170737c25a
Zikai Liu <zikai.liu@outlook.com>
2023-07-09 11:11:09 +0200

    Clean up scripts

--- scripts/render-scss.js
+++ scripts/render-scss.js
@@ -10,33 +10,29 @@ const sh = require('shelljs');
 const stylesPath = '../src/scss/styles.scss';
 const destPath = upath.resolve(upath.dirname(__filename), '../css/styles.full.css');
 
-module.exports = function renderSCSS() {
-
-    const results = sass.renderSync({
-        data: entryPoint,
-        includePaths: [
-            upath.resolve(upath.dirname(__filename), '../node_modules')
-        ],
-      });
-
-    const destPathDirname = upath.dirname(destPath);
-    if (!sh.test('-e', destPathDirname)) {
-        sh.mkdir('-p', destPathDirname);
-    }
-
-    postcss([ autoprefixer ]).process(results.css, {from: 'styles.css', to: 'styles.css'}).then(result => {
-        result.warnings().forEach(warn => {
-            console.warn(warn.toString())
-        })
-        fs.writeFileSync(destPath, result.css.toString());
-    })
-
-};
-
 const entryPoint = `/*!
  * CSS for my portfolio. See styles.full.js for the unminified version.
  * Copyright 2022 Zikai Liu
  * Reference: Start Bootstrap - Grayscale v7.0.5 (https://startbootstrap.com/theme/grayscale, Licensed under MIT)
  */
 @import "${stylesPath}"
-`
+`;
+
+const results = sass.renderSync({
+    data: entryPoint,
+    includePaths: [
+        upath.resolve(upath.dirname(__filename), '../node_modules')
+    ],
+});
+
+const destPathDirname = upath.dirname(destPath);
+if (!sh.test('-e', destPathDirname)) {
+    sh.mkdir('-p', destPathDirname);
+}
+
+postcss([ autoprefixer ]).process(results.css, {from: 'styles.css', to: 'styles.css'}).then(result => {
+    result.warnings().forEach(warn => {
+        console.warn(warn.toString())
+    })
+    fs.writeFileSync(destPath, result.css.toString());
+});
